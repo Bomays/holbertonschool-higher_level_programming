f3f2b162eb0ca767e8df7dd6d0c77860a2b6abe8
Bomays <9314@holbertonstudents.com>
2024-09-23 15:33:29 +0200

    added test and isinstance typeError loop

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -11,4 +11,8 @@ class MyList(list):
 
     def print_sorted(self):
         '''Prints sorted list'''
+        for element in self:
+            if not isinstance(element, int):
+                raise TypeError("list elements must be integer")
+            
         print(sorted(self))
