f9670baf6023e048c89cb48baabe4341e8636005
Bomays <9314@holbertonstudents.com>
2024-09-23 16:30:46 +0200

    modified == to 'is' in return class type checking

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -7,7 +7,7 @@ This module checks the class type
 def is_same_class(obj, a_class):
     """
     Returns True if the object is
-    exactly an instance of the specified class ;
+    exactly an instance of the specified class
     otherwise False
     """
-    return type(obj) == a_class
+    return type(obj) is a_class
