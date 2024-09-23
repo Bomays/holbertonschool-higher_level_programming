4e4eef6fbd66d10d7186625759c4909802785049
Bomays <9314@holbertonstudents.com>
2024-09-23 14:39:34 +0200

    added init

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -7,6 +7,10 @@ class MyList(list):
     """
     Built-in list
     """
+    def __init_(self):
+        '''Initialize the class callin superclass'''
+        super().__init__()
+
     def print_sorted(self):
             '''Prints sorted list'''
             print(sorted(self))
