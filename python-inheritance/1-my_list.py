c5d4d7515373b606f6567f31f6757f09a0e41090
Bomays <9314@holbertonstudents.com>
2024-09-23 15:31:07 +0200

    added tests

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,14 +3,12 @@
 This module defines a list
 """
 
+
 class MyList(list):
     """
     Built-in list
     """
-    def __init_(self):
-        '''Initialize the class callin superclass'''
-        super().__init__()
 
     def print_sorted(self):
-            '''Prints sorted list'''
-            print(sorted(self))
+        '''Prints sorted list'''
+        print(sorted(self))
