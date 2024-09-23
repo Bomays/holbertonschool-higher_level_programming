126448b84a09ff987418297d5afefd43dceeef4c
Bomays <9314@holbertonstudents.com>
2024-09-23 15:52:37 +0200

    pycodestyle corrected

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -8,11 +8,10 @@ class MyList(list):
     """
     Built-in list
     """
-
     def print_sorted(self):
-        '''Prints sorted list'''
+        """Prints sorted list"""
         for element in self:
             if not isinstance(element, int):
                 raise TypeError("list elements must be integer")
-       
+
         print(sorted(self))
