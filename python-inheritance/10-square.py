cbe509c4b3b02ea3561d17b54a6e6a86b997640f
Bomays <9314@holbertonstudents.com>
2024-09-24 21:48:02 +0200

    doc modif

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -4,26 +4,19 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """
-    Subclass inherited of rectangle subclass that defines a square
-
-    Attributes:
-        __size(int): size (length) of a square
-    """
+    """Subclass inherited of rectangle subclass that defines a square"""
 
     def __init__(self, size):
         """Initializing square instance
 
         Args:
-        size(int): zie of a square
-
-        Raises:
-            TypeError: if value is not an integer
-            ValueError: if value is less than 0 or equal to 0
+            width(): integer
+            height(): integer
         """
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
+        """ Initialize Rectangle width and height with size"""
 
     def area(self):
         """Method that calculates a rectangle area
@@ -35,7 +28,7 @@ class Square(Rectangle):
 
     def __str__(self):
         """Method with __str__ that returns a string
-        representation of a square
+            representation of a square
 
         Returns:
             str: informal string representation of a square
