c6cae57f11e4a1d708672bbf98f4a1b3830d587d
Bomays <9314@holbertonstudents.com>
2024-09-24 15:14:39 +0200

    added class rectangle inherited from superclass with value validator handled

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+This module defines base geometry
+"""
+
+
+class BaseGeometry:
+    """ Class that defines base of geometry"""
+
+    def area(self):
+        """Raise exception with a message"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Instance method that validates value
+
+        Args:
+            name(str): name of parameter
+            value(int): value to validate
+
+        Raises:
+            TypeError: if value isnot an integer
+            ValueError: if value is less than 0 or equals 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """
+    Class inherited from superclass BaseGeometry
+    that defines a rectangle
+    """
+
+    def __init__(self, width, height):
+        """Initializing method"""
+
+        self.integer_validator("width", width)
+        self.__width = width
+
+        self.integer_validator("height", height)
+        self.__height = height
