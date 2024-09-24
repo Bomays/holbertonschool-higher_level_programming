f486475df596cd5be3d36a872978ea0755ad1577
Bomays <9314@holbertonstudents.com>
2024-09-24 14:25:50 +0200

    modified if isinstance by type

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -20,7 +20,7 @@ class BaseGeometry:
         Raises:
             TypeError and ValueError
         """
-        if not isinstance(value, int):
+        if not type is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
