e6715e3852bef8b0969aff56556da9136bdfa3c8
Bomays <9314@holbertonstudents.com>
2024-09-24 10:38:15 +0200

    added Area function that raises an exception with a message

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module defines base geometry
+"""
+
+
+class BaseGeometry:
+    """ Class that defines base of geometry"""
+    def area(self):
+        """Raise exception with a message"""
+        raise Exception("area() is not implemented")
