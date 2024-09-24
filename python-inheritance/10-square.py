21ef26cce2e5844ded8a25c0af6d39c84fb64e4f
Bomays <9314@holbertonstudents.com>
2024-09-24 21:10:28 +0200

    corrected syntax in the import

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """A class Square which is a Rectangle heritage"""
-Rectangle = __import__('9-retangle').Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
