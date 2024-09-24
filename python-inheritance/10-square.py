5f0911599394bf0e32fd340425c49a130e72cf68
Bomays <9314@holbertonstudents.com>
2024-09-24 22:20:22 +0200

    doc modif and syntax and area error in calculation

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -7,30 +7,23 @@ class Square(Rectangle):
     """Subclass inherited of rectangle subclass that defines a square"""
 
     def __init__(self, size):
-        """Initializing square instance
+        """Initializing square instance"""
 
-        Args:
-            width(): integer
-            height(): integer
-        """
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
-        """ Initialize Rectangle width and height with size"""
 
     def area(self):
         """Method that calculates a rectangle area
-
         Returns:
             int: square area
         """
-        return (self.__size * 2)
+        return self.__size ** 2
 
     def __str__(self):
         """Method with __str__ that returns a string
             representation of a square
-
         Returns:
             str: informal string representation of a square
         """
-        return ("[Rectangle] {}/{}".format(self.__size, self.__size))
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
