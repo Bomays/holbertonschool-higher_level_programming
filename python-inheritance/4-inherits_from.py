f764ac0aa24ac6504439630163a760da9fa70176
Bomays <9314@holbertonstudents.com>
2024-09-24 09:02:10 +0200

    pycodestyle correction

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -6,7 +6,7 @@ This module is an inheritance checker
 
 def inherits_from(obj, a_class):
     """
-    Checking if object is a class instance inherited 
+    Checking if object is a class instance inherited
     directly or indirectly from the specified class
     """
     return isinstance(obj, a_class) and type(obj) is not a_class
