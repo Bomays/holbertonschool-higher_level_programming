5287748694ca3244d9344675e208a543ad609bb6
Bomays <9314@holbertonstudents.com>
2024-09-24 17:28:38 +0200

    added area forgotten test and doc modif

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,24 +1,30 @@
 #!/usr/bin/python3
 """
-This module defines base geometry
+This module defines base geometry classes
 """
 
 
 class BaseGeometry:
     """ Class that defines base of geometry"""
+
     def area(self):
-        """Raise exception with a message"""
+        """Method that calculates a rectangle area
+
+        Raise:
+            Exception: Raise exception with a message
+            "area() is not implemented"
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Instance method that validates value
+        """Validates that value is a positive integer
 
         Args:
             name(str): name of parameter
             value(int): value to validate
 
         Raises:
-            TypeError: if value isnot an integer
+            TypeError: if value is not an integer
             ValueError: if value is less than 0 or equals 0
         """
         if not isinstance(value, int):
