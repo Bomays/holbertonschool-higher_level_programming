3024563f561e4597ec5461e15fb1fbffda722a71
Bomays <9314@holbertonstudents.com>
2024-09-25 21:15:58 +0200

    modifed dragon instantiation test

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -4,29 +4,35 @@ using Mixin classes """
 
 
 class SwimMixin:
+    """Mixin class for dragon swimming"""
+
     def swim(self):
         print("The creature swims!")
 
 
 class FlyMixin:
+    """Mixin class for dragon flying"""
+
     def fly(self):
         print("The creature flies!")
 
 
 class Dragon(SwimMixin, FlyMixin):
+    """Inherited class for dragon roar"""
+
     def __init__(self, name):
+        """Initialize dragon with name"""
         self.name = name
 
     def roar(self):
         print("The dragon roars!")
 
-    def who_roars(self):
-        print("{} the dragon roars!".format(self.name))
 
+"""TESTS"""
 
 draco = Dragon("draco")
+"""Instantiation of Dragon"""
 
 draco.swim()
 draco.fly()
 draco.roar()
-draco.who_roars()
