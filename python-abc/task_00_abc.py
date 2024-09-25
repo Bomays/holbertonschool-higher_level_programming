22b5fa039c2d54f998e76321d71488b183061dba
Bomays <9314@holbertonstudents.com>
2024-09-25 11:08:48 +0200

    added abstract class and abstract method implemented in its subclasses

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Module Animal_sound using abstract class and method to defines the sound of a dog or a cat"""
+
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    """Abstract class reprsenting an animal"""
+
+    @abstractmethod
+    def sound(self):
+        """animal sound abstract method implemanted in subclasses"""
+        pass
+
+
+class Dog(Animal):
+    """Animal subclass for a dog"""
+
+    def sound(self):
+        """Return dog sound"""
+        return "Bark"
+
+
+class Cat(Animal):
+    """Animal subclass for a cat"""
+
+    def sound(self):
+        """Return cat sound"""
+        return "Meow"
