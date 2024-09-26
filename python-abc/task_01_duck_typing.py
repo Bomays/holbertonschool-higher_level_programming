2a8abba92ec8db061303c1391f08b8054b69c304
Bomays <9314@holbertonstudents.com>
2024-09-26 16:39:16 +0200

    added abs() to radius of circle to have abosolute result

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -19,13 +19,13 @@ class Shape(ABC):
 
 class Circle(Shape):
     def __init__(self, radius):
-        self.radius = radius
+        self.radius = abs(radius)
 
     def area(self):
-        return math.pi * (self.radius**2)
+        return abs(math.pi * (self.radius**2))
 
     def perimeter(self):
-        return 2 * math.pi * self.radius
+        return abs(2 * math.pi * self.radius)
 
 
 class Rectangle(Shape):
