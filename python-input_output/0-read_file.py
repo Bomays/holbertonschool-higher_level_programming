b0412ff803f10bdbc0591129e6a9f94cec9f98ca
Bomays <9314@holbertonstudents.com>
2024-09-30 14:34:10 +0200

    pycodestyle > added one more blank line after Shebang

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines reading utf-8 text file process"""
 
+
 def read_file(filename=""):
     """Reads and write UTF-8 text file thanks to 'With' statement"""
     with open(filename, 'r', encoding='utf-8') as f:
