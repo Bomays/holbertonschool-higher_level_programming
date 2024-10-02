05fd0bc3f9fa5e8640c4e541e0929f7816f3c4df
Bomays <9314@holbertonstudents.com>
2024-10-02 18:46:13 +0200

    added function that returns a dic. representation of Student instance. If attrs is a list of strings, only attribute names contained in this list must be retrieved. Otherwise, all attributes must be retrieved.

---
+++ python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This module defines a class student defining a student"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """initialize Student with attributes"""
+        self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def to_json(self, attrs=None):
+        """Returns a dic. representation of Student instance
+        If attrs is a list of strings, only attribute names
+        contained in this list must be retrieved.
+        Otherwise, all attributes must be retrieved."""
+        if isinstance(attrs, list):
+            return {
+                attr: self.__dict__[attr]
+                for attr in attrs if attr in self.__dict__
+            }
+        else:
+            return self.__dict__
