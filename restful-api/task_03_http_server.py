be17cf176525f7b0a44401342af554a47e3da631
Bomays <9314@holbertonstudents.com>
2024-10-12 17:11:13 +0200

    added / endpoint handling

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -6,7 +6,6 @@ import http.server
 import socketserver
 import json
 
-
 PORT = 8000
 
 
@@ -16,7 +15,20 @@ class MyFirstServer(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         """do_GET method to manage endpoints handling"""
 
-        if self.path == "/data":
+        if self.path == "/":
+            """/ endpoint handling"""
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+
+            message = "Hello, this is a simple API!"
+            self.wfile.write(message.encode("utf-8"))
+            self.wfile.flush()
+            """ensure to send data direcly after self.wfile.write
+            call by flushing > must avoid some BrokenPipeError
+            """
+
+        elif self.path == "/data":
             """/data endpoint handling"""
             self.send_response(200)
             self.send_header("Content-type", "application/json")
@@ -25,9 +37,6 @@ class MyFirstServer(http.server.BaseHTTPRequestHandler):
             data = {"name": "John", "age": 30, "city": "New York"}
             self.wfile.write(json.dumps(data).encode("utf-8"))
             self.wfile.flush()
-            """ensure to send data direcly after self.wfile.write
-            call by flushing > must avoid some BrokenPipeError
-            """
 
         elif self.path == "/status":
             """/status endpoint handling"""
