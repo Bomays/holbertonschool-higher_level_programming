d5556ac0a149093abb5fd23c59c926cfeddba466
Bomays <9314@holbertonstudents.com>
2024-10-14 22:42:16 +0200

    added function doc and simplified variable

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -28,47 +28,52 @@ users = {
 
 @app.route("/", methods=["GET"])
 def home():
+    """Return welcome message"""
     return "Welcome to the Flask API!"
 
 
 @app.route("/data", methods=["GET"])
 def get_json_data():
+    """Return a list of usernames in JSON format"""
     return jsonify(list(users.keys()))
 
 
 @app.route("/status", methods=["GET"])
 def status():
+    """Return status message"""
     return "OK"
 
 
 @app.route("/users/<username>", methods=["GET"])
 def get_user(username):
+    """Return the user object for the given username"""
     user = users.get(username)
 
     if user:
         return jsonify(user)
     else:
-        return jsonify({"error": "User not found"})
+        return jsonify({"error": "User not found"}), 404
 
 
 @app.route("/add_user", methods=["POST"])
 def add_user():
+    """Add a new user to the dict."""
     data = request.get_json()
     username = data.get("username")
 
     if not username:
         return jsonify({"error": "Username is required"}), 400
 
-    new_user_added = {
+    new_user = {
         "username": username,
         "name": data.get("name"),
         "age": data.get("age"),
         "city": data.get("city"),
     }
 
-    users[username] = new_user_added
+    users[username] = new_user
 
-    return jsonify({"message": "User added", "user": new_user_added}), 200
+    return jsonify({"message": "User added", "user": new_user}), 200
 
 
 if __name__ == "__main__":
