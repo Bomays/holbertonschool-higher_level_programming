b1a09697b13e58286b40a82f43b6ea4237a659eb
Bomays <9314@holbertonstudents.com>
2024-10-14 17:49:10 +0200

    added my first Flask API testing add_user with python text file

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""A first Flask API """
+
+
+from pkgutil import get_data
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+users = {
+    "jane": {"username": "jane", "name": "Jane", "age": 28, "city": "Los Angeles"},
+    "john": {"username": "john", "name": "John", "age": 30, "city": "New York"},
+}
+
+
+@app.route("/", methods=["GET"])
+def home():
+    return "<p>Welcome to the Flask API!</p>"
+
+
+@app.route("/data", methods=["GET"])
+def get_json_data():
+    return jsonify(list(users.keys()))
+
+
+@app.route("/status", methods=["GET"])
+def status():
+    return "<p>OK</p>"
+
+
+@app.route("/users/<username>", methods=["GET"])
+def get_user(username):
+    user = users.get(username)
+
+    if user:
+        return jsonify(user)
+    else:
+        return jsonify({"error": "User not found"})
+
+
+@app.route("/add_user", methods=["POST"])
+def add_user():
+    data = request.get_json()
+    username = data.get("username")
+
+    if (
+        not username
+        or not data
+        or not data.get("name")
+        or not data.get("age")
+        or not data.get("city")
+    ):
+        return jsonify({"error": "Username is required"}), 400
+
+    users[username] = {
+        "username": username,
+        "name": data.get("name"),
+        "age": data.get("age"),
+        "city": data.get("city"),
+    }
+
+    return jsonify({"message": "User added"}), 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+"""debug=True for flask automatisation and detailed errors"""
