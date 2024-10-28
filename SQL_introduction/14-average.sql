a6ff6484a4fc3e351773d3e3f36e1901ca1dd45f
Bomays <9314@holbertonstudents.com>
2024-10-28 22:34:43 +0100

    Modified script to have "average" as column name

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -2,4 +2,4 @@
 -- 14-average.sql
 -- Script that computes the score average of all records in the table second_table of the database hbtn_0c_0 in MySQL
 
-SELECT AVG(score) FROM second_table
+SELECT AVG(score) AS average FROM second_table;
