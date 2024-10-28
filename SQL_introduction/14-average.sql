847c590fb344f2a1501ed78c16b4bc50c331be4b
Bomays <9314@holbertonstudents.com>
2024-10-28 21:06:02 +0100

    Added sql script for: computing the score average of all records in the table with SELECT...FROM, listing number of records with same score in DESC orderusing SELECT...COUNT AS... FROM...GROUP BY...ORDER BY, and finally listing by DESC score all records of the table without empty or null name value

---
+++ SQL_introduction/14-average.sql
@@ -0,0 +1,5 @@
+#!/bin/bash
+-- 14-average.sql
+-- Script that computes the score average of all records in the table second_table of the database hbtn_0c_0 in MySQL
+
+SELECT AVG(score) FROM second_table
