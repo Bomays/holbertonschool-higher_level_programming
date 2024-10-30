4a736e87af80537e48e4010f4294d8f5476e84f7
Bomays <9314@holbertonstudents.com>
2024-10-30 18:34:30 +0100

    trying to correct my query syntax

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -4,5 +4,5 @@
 
 USE `hbtn_0d_usa`
 SELECT `id`, `name` FROM `cities`
-WHERE `state_id` = (SELECT `id` FROM `states` WHERE `name` = "California") 
-ORDER BY `id` ASC;
+WHERE `cities`.`state_id` = (SELECT `id` FROM `states` WHERE `name` = "California") 
+ORDER BY `cities`.`id` ASC;
