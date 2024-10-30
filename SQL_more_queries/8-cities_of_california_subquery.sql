7ed37d125074498ae5d43cbde2bd2fef10490e47
Bomays <9314@holbertonstudents.com>
2024-10-30 18:38:46 +0100

    deleted USE "databse_name" first line

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,7 +2,6 @@
 -- 8-cities_of_california_subquery.sql
 -- Script that list all cities of California that can be found in database hbtn_0d_usa
 
-USE `hbtn_0d_usa`
 SELECT `id`, `name` FROM `cities`
 WHERE `cities`.`state_id` = (SELECT `id` FROM `states` WHERE `name` = "California") 
 ORDER BY `cities`.`id` ASC;
