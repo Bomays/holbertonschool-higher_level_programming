7757e770b6f02208ff35efb77dd0341f8cf6ac20
Bomays <9314@holbertonstudents.com>
2024-10-30 19:09:30 +0100

    corrected columns name

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -3,8 +3,8 @@
 -- Script that list all cities contained in the database hbtn_0d_usa
 
 SELECT cities.id,
-cities.name AS city_name,
-states.name AS state_name
+cities.name AS name,
+states.name AS name
 FROM cities
 JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
