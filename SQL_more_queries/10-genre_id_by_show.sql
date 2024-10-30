24afa501af2841d6fa9f00046c685794f0519db2
Bomays <9314@holbertonstudents.com>
2024-10-30 20:01:51 +0100

    removed USE <database>

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -2,7 +2,6 @@
 -- 10-genre_id_by_show.sql
 -- Script that list all shows contained in the database hbtn_0d_tvshows
 
-USE `hbtn_0d_tvshows`
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
