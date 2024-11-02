ae9f48cc75689d5f2d3e0d04ca405d2710f00628
Bomays <9314@holbertonstudents.com>
2024-11-02 12:12:30 +0100

    rearrange script to have connect function with doc after import and befor entrypoint

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,7 @@ it returns all states with a name starting with N (upper case)
 sorted in ascending order by states.id
 
 NB: cursor.close() and db.close() are ensured
-by using with...as.. statement instead
+by using with...as.. statement instead.
 
 Usage:
     python3 1-filter_states.py
@@ -27,17 +27,23 @@ import MySQLdb
 from sys import argv
 
 
-if __name__ == "__main__":
-    """Main entry point"""
-
-
 def connect_database(username, password, db_name):
     """
-    Connects to the database using 3 args, returns MySQLdb.connection
+    Args:
+        username (str): mysql username
+        password (str): mysql password
+        database_name (str): database name
+
+    Returns:
+        Returns MySQL connection to database
     """
     return MySQLdb.connect(
         host="localhost", port=3306, user=username, passwd=password, db=db_name
     )
+
+
+if __name__ == "__main__":
+    """Main entry point"""
 
 
 with connect_database(argv[1], argv[2], argv[3]) as db:
