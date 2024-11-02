db8a9aaed585b66fb94c460602d4d9892944312b
Bomays <9314@holbertonstudents.com>
2024-11-02 11:36:47 +0100

    Added properly documentation and added a databse connection function berfore entry point

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,29 +1,49 @@
 #!/usr/bin/python3
-"""Module that lists all states from database 
+"""
+Module that lists all states from database
+
 after connecting to MySQL server running on localhost
-at port 3306 to access database hbtn_0e_0_usa"""
+at port 3306 to access database hbtn_0e_0_usa,
+it returns all states from database
+sorted in ascending order by states.id
+
+
+Usage:
+    python3 0-select_states.py
+    <mysql_username> <mysql_password> <database_name>
+
+Args :
+    mysql_username: argv[1]
+    mysql_password: argv[2]
+    database_name: argv[3]
+
+Returns:
+    Prints all states from the database
+"""
 
 import MySQLdb
 from sys import argv
 
 
-if __name__=="__main__":
-
-
-    """ 
-    Args: 
-        mysql_username: user = sys.argv[1],
-        mysql_password: passwd = sys.argv[2],
-        database_name: db = sys.argv[3],
-
-    Return:
-        returns all states from databse hbtn_0e_0_usa,
-        sorted in ascending order by states.id
+def connect_database(username, password, db_name):
     """
+    Args:
+        username (str): mysql username
+        password (str): mysql password
+        database_name (str): database name
 
-    with MySQLdb.connect(
-        host="localhost", port=3306,
-        user=argv[1], passwd=argv[2], db=argv[3]) as db:
+    Returns:
+        Returns MySQL connection to database
+    """
+    return MySQLdb.connect(
+        host="localhost", port=3306, user=username, passwd=password, db=db_name
+    )
+
+
+if __name__ == "__main__":
+    """Main entry point"""
+
+    with connect_database(argv[1], argv[2], argv[3]) as db:
 
         # ensuring query executes well  by using cursor object
         cursor = db.cursor()
