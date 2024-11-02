910379ad58247d39ded7722f1ca64f1890592f12
Bomays <9314@holbertonstudents.com>
2024-11-02 21:00:56 +0100

    added port 3306

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Module that lists all the cities from database hbtn_0e_6_usa,
+Module that lists all the cities from database hbtn_0e_6_usa, 
 via MySQLAlchemy
 """
 
@@ -11,20 +11,20 @@ from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 
-username = argv[1]
-password = argv[2]
-database_name = argv[3]
-
 if __name__ == "__main__":
     """Main entry point"""
 
+    username = argv[1]
+    password = argv[2]
+    database_name = argv[3]
+
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
             argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
-    session = Session()
+    session= Session()
 
     for state in session.query(State).order_by(State.id).all():
         print('{}: {}'.format(state.id, state.name))
