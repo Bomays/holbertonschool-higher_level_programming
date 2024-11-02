ee354305ff47e047733d61096ff14b6902fc7050
Bomays <9314@holbertonstudents.com>
2024-11-02 23:26:01 +0100

    Added task 13: Module that deletes all State objects with a name containing the letter 'a' from the database hbtn_0e_6_usa, via MySQLAlchemy

---
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Module that deletes all State objects with a name containing
+the letter 'a' from the database hbtn_0e_6_usa, via MySQLAlchemy
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    """Main entry point"""
+
+    username = argv[1]
+    password = argv[2]
+    database_name = argv[3]
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            argv[1], argv[2], argv[3]),
+        pool_pre_ping=True,
+    )
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).filter(
+        State.name.like('%a%')).order_by(State.id).all()
+
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+        session.delete(state)
+
+    session.commit()
+    session.close()
