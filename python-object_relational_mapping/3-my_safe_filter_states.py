e0b75a44c67b868051e2a1eaacf2a4e9bddd94f8
Bomays <9314@holbertonstudents.com>
2024-11-02 15:52:35 +0100

    Added Module that retrieves the state name which is safe of any MySQL injections

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+Module that retrieves the state name which is safe of any MySQL injections
+
+After connecting to MySQL server running on localhost
+at port 3306 to access database hbtn_0e_0_usa,
+it returns the state name wich is safe of any MySQL injections
+
+Usage:
+    python3 3-my_safe_filter_states.py
+    <mysql_username> <mysql_password> <database_name> <safe_state_name>
+
+Args :
+    mysql_username: argv[1]
+    mysql_password: argv[2]
+    database_name: argv[3]
+    state_name_searched: argv[4]
+
+Returns:
+    Prints state which is safe of any MySQL injections
+"""
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    """Main entry point"""
+
+    username = argv[1]
+    password = argv[2]
+    database_name = argv[3]
+    state_name_searched = argv[4]
+
+    with MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database_name
+    ) as db:
+
+        with db.cursor() as cursor:
+            query = """SELECT * FROM states
+                    WHERE name = '{:s}'
+                    COLLATE utf8mb4_bin
+                    ORDER BY states.id ASC""".format(argv[4])
+            cursor.execute(query)
+
+            states = cursor.fetchall()
+            for state in states:
+                print(state)
