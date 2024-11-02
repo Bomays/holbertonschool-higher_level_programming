f6842462852afb2cac9effd1baa394c1ee9d316e
Bomays <9314@holbertonstudents.com>
2024-11-02 15:17:30 +0100

    changed script deleting function and initializing args

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -7,8 +7,8 @@ at port 3306 to access database hbtn_0e_0_usa,
 it returns the state name wich is safe of any MySQL injections
 
 Usage:
-    python3 1-filter_states.py
-    <mysql_username> <mysql_password> <database_name> <safe_name>
+    python3 2-my_filter_states.py
+    <mysql_username> <mysql_password> <database_name> <safe_state_name>
 
 Args :
     mysql_username: argv[1]
@@ -24,31 +24,26 @@ import MySQLdb
 from sys import argv
 
 
-def connect_database(username, password, db_name):
-    """
-    Args:
-        username (str): mysql username
-        password (str): mysql password
-        database_name (str): database name
-
-    Returns:
-        Returns MySQL connection to database
-    """
-    return MySQLdb.connect(
-        host="localhost", port=3306, user=username, passwd=password, db=db_name
-    )
-
-
 if __name__ == "__main__":
     """Main entry point"""
 
-    with connect_database(argv[1], argv[2], argv[3]) as db:
+    username = argv[1]
+    password = argv[2]
+    database_name = argv[3]
+    state_name_searched = argv[4]
+
+    with MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database_name
+    ) as db:
+
         with db.cursor() as cursor:
-            query = "SELECT * FROM states\
+            query = ("SELECT * FROM states\
                     WHERE name = '{}'\
-                    ORDER BY states.id ASC".format(
-                argv[4]
-            )
+                    ORDER BY states.id ASC".format(argv[4]))
 
             cursor.execute(query)
             for state in cursor.fetchall():
