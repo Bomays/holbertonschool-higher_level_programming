6e1d63220f982d65cbf19f5c5d5016f811a1b49a
Bomays <9314@holbertonstudents.com>
2024-11-02 15:43:02 +0100

    added search case-sensitive collation to ensure it considers the case model of the arg

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -43,6 +43,7 @@ if __name__ == "__main__":
         with db.cursor() as cursor:
             query = """SELECT * FROM states
                     WHERE name = '{:s}'
+                    COLLATE latin1_general_cs
                     ORDER BY states.id ASC""".format(argv[4])
             cursor.execute(query)
 
