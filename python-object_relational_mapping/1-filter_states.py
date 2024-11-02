5104a5c107a3e72d0a78532f8903f204a5643e36
Bomays <9314@holbertonstudents.com>
2024-11-02 12:08:37 +0100

    pycodestyled my script and formated it

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,7 @@ it returns all states with a name starting with N (upper case)
 sorted in ascending order by states.id
 
 NB: cursor.close() and db.close() are ensured
-by using with...as.. statement instead 
+by using with...as.. statement instead
 
 Usage:
     python3 1-filter_states.py
@@ -36,14 +36,15 @@ def connect_database(username, password, db_name):
     Connects to the database using 3 args, returns MySQLdb.connection
     """
     return MySQLdb.connect(
-        host="localhost", port=3306,
-        user=username, passwd=password, db=db_name
+        host="localhost", port=3306, user=username, passwd=password, db=db_name
     )
 
-with connect_database(argv[1], argv[2], argv[3]) as db:
 
+with connect_database(argv[1], argv[2], argv[3]) as db:
     with db.cursor() as cursor:
-        cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
+        cursor.execute(
+            "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC"
+        )
 
         for state in cursor.fetchall():
             print(state)
