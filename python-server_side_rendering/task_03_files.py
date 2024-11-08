7087a8323bc1c5633af47059ce8667a88f9ff29c
Bomays <9314@holbertonstudents.com>
2024-11-09 00:56:52 +0100

    corrected jinja syntax and put integer for p

--- python-server_side_rendering/task_03_files.py
+++ python-server_side_rendering/task_03_files.py
@@ -1,4 +1,5 @@
 import json
+import csv
 import os
 from flask import Flask, render_template, request
 
@@ -42,29 +43,31 @@ def products():
     source = request.args.get("source")
     product_id = request.args.get("id")
 
-    file_paths = {"json": "prodcuts.json", "csv": "products.csv"}
-    file_path = file_paths.get(source)
+    file_path = ""
 
-    if source not in file_paths:
+    if source == "json":
+        file_path = "products.json"
+
+    elif source == "csv":
+        file_path = "products.csv"
+
+    else:
         return render_template("product_display.html", error="Wrong source")
 
-    file_path = file_paths[source]
 
     if not os.path.exists(file_path):
         return render_template("product_display.html", error="File not found")
 
-    products = read_json(file_path) if source == "json" else read_csv(file_path)
+    if source == "json":
+        products = read_json(file_path)
+    else:
+        products = read_csv(file_path)
 
     if product_id:
-        try:
-            product_id = int(product_id)
-            products = [p for p in products if p["id"] == product_id]
-            if not products:
-                return render_template(
-                    "product_display.html", error="File not found")
-        except ValueError:
-            return render_template(
-                "product_display.html", error="Invalid format")
+        product_id = int(product_id)
+        products = [p for p in products if p['id'] == product_id]
+        if not products:
+            return render_template("product_display.html", error="Product not found")
 
     return render_template("product_display.html", products=products)
 
