92bf403d4bdf03523f83f4678572e43c33e9293b
Bomays <9314@holbertonstudents.com>
2024-11-08 20:41:04 +0100

    added return for errors, and ensure that value of placeholder is a string

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -17,17 +17,20 @@ def generate_invitations(template, attendees):
 
     if not isinstance(template, str):
         print(f"Template must be a string, not {type(template).__name__}")
+        return
 
     if not attendees: 
         print("You must fill attendees with a list of dictionnaries")
+        return
 
     if not isinstance(attendees, list):
         print(f"Attendees must be a list, no {type(attendees).__name__}")
+        return
 
     if not all (isinstance(item, dict) for item in attendees):
         invalid_item = [type(item.__name__ for item in attendees if not isinstance(item, dict))]
         print("Item in attendees must be a dictionary, not {invalid_item}")
-
+        return
 
     for index, attendee in enumerate(attendees, start=1):
 
@@ -37,7 +40,7 @@ def generate_invitations(template, attendees):
 
             placeholder =  '{' + key + '}'
             value = attendee.get(key, "N/A")
-            invitation = invitation.replace(placeholder, value)
+            invitation = invitation.replace(placeholder, str(value))
 
             filename = f"output_{index}.txt"
 
